62bf64eb9818b1de6479b31246b88d379a77fcb7
renzgonzales <145319475+renzgonzales@users.noreply.github.com>
2024-02-23 00:40:06 +0800

    Add files via upload

---
+++ CARRENZ.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrenz;
+
+/**
+ *
+ * @author User
+ */
+public class CARRENZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CARRENZ_RAW1 myCar = new CARRENZ_RAW1(" P A G A N I ", " Horacio Pagani " ,1992);
+        
+        myCar.PrintDetails();
+    }
+    
+}
